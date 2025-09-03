35acfdb166050f5904241e7bb47fa704cd4fbbec
kim20598 <ki688494@gmail.com>
2025-09-03 18:26:00 +0100

    Update NOXXProvider.kt

--- NOXXProvider/src/main/kotlin/com/Phisher98/NOXXProvider.kt
+++ NOXXProvider/src/main/kotlin/com/Phisher98/NOXXProvider.kt
@@ -1,14 +1,18 @@
 package com.phisher98
 
+import com.lagradost.api.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
 import com.lagradost.cloudstream3.network.DdosGuardKiller
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
+import com.lagradost.nicehttp.NiceResponse
 import okhttp3.FormBody
-import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
-class NOXXProvider : MainAPI() {
+class NOXXProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://noxx.to"
     override var name = "NOXX"
     override val hasMainPage = true
@@ -19,7 +23,7 @@ class NOXXProvider : MainAPI() {
     )
     private var ddosGuardKiller = DdosGuardKiller(true)
 
-    private suspend fun queryTVApi(count: Int, query: String): org.jsoup.nodes.Document {
+    private suspend fun queryTVApi(count: Int, query: String): NiceResponse {
         val body = FormBody.Builder()
             .addEncoded("no", "$count")
             .addEncoded("gpar", query)
@@ -27,17 +31,16 @@ class NOXXProvider : MainAPI() {
             .addEncoded("spar", "series_added_date desc")
             .build()
 
-        val response = app.post(
+        return app.post(
             "$mainUrl/fetch.php",
             requestBody = body,
             interceptor = ddosGuardKiller,
             referer = "$mainUrl/"
         )
-        return Jsoup.parse(response.text)
     }
 
-    private suspend fun queryTVsearchApi(query: String): org.jsoup.nodes.Document {
-        val response = app.post(
+    private suspend fun queryTVsearchApi(query: String): NiceResponse {
+        return app.post(
             "$mainUrl/livesearch.php",
             data = mapOf(
                 "searchVal" to query
@@ -45,42 +48,45 @@ class NOXXProvider : MainAPI() {
             interceptor = ddosGuardKiller,
             referer = "$mainUrl/"
         )
-        return Jsoup.parse(response.text)
     }
 
     private val scifiShows = "Sci-Fi"
     private val advenShows = "Adventure"
     private val actionShows = "Action"
-    private val dramaShows = "Drama"
+    private val horrorShows = "Horror"
+    private val DramaShows= "Drama"
     private val comedyShows = "Comedy"
     private val fantasyShows = "Fantasy"
+    private val romanceShows = "Romance"
 
     override val mainPage = mainPageOf(
+        //TV Shows
         scifiShows to scifiShows,
         advenShows to advenShows,
         actionShows to actionShows,
         comedyShows to comedyShows,
         fantasyShows to fantasyShows,
-        dramaShows to dramaShows,
+        DramaShows to DramaShows,
     )
 
     override suspend fun getMainPage(
         page: Int,
         request: MainPageRequest
     ): HomePageResponse {
-        val itemsPerPage = 48
-        val offset = page * itemsPerPage
-        val document = queryTVApi(offset, request.data)
-        val home = document.select("a.block").mapNotNull {
+        val query = request.data.format(page)
+        val TVlist = queryTVApi(
+            page * 48,
+            query
+        ).document
+        val home = TVlist.select("a.block").mapNotNull {
             it.toSearchResult()
         }
-        val hasNext = home.size >= itemsPerPage
-        return HomePageResponse(listOf(HomePageList(request.name, home)), hasNext = hasNext)
+        return newHomePageResponse(arrayListOf(HomePageList(request.name, home)), hasNext = true)
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("div > div > span")?.text()?.trim() ?: return null
-        val href = fixUrl(this.attr("href"))
+        val title = this.selectFirst("div > div > span")?.text()?.toString()?.trim() ?: return null
+        val href = fixUrl(this.attr("href").toString())
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-src"))
         val quality = SearchQuality.HD
 
@@ -91,52 +97,49 @@ class NOXXProvider : MainAPI() {
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val document = queryTVsearchApi(query)
-        return document.select("a[href^=\"/tv\"]").mapNotNull {
-            val title = it.selectFirst("div > h2")?.text()?.trim() ?: return@mapNotNull null
-            val href = fixUrl("$mainUrl${it.attr("href")}")
+        val TVlist = queryTVsearchApi(
+            query
+        ).document
+        return TVlist.select("a[href^=\"/tv\"]").mapNotNull {
+            val title = it.selectFirst("div > h2")?.text().toString().trim()
+            val href = fixUrl(mainUrl + it.attr("href").toString())
             val posterUrl = fixUrlNull(it.selectFirst("img")?.attr("src"))
+            val quality = SearchQuality.HD
 
             newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
                 this.posterUrl = posterUrl
-                this.quality = SearchQuality.HD
+                this.quality = quality
             }
         }
     }
 
     override suspend fun load(url: String): LoadResponse? {
         val doc = app.get(url, interceptor = ddosGuardKiller).document
-        val title = doc.selectFirst("h1.px-5")?.text()?.trim() ?: return null
+        val title = doc.selectFirst("h1.px-5")?.text()?.toString()?.trim() ?: return null
         val poster = fixUrlNull(doc.selectFirst("img.relative")?.attr("src"))
         val tags = doc.select("div.relative a[class*=\"py-0.5\"]").map { it.text() }
         val description = doc.selectFirst("p.leading-tight")?.text()?.trim()
         val rating = doc.select("span.text-xl").text().toRatingInt()
         val actors = doc.select("div.font-semibold span.text-blue-300").map { it.text() }
-        val recommendations = doc.select("a.block").mapNotNull { it.toSearchResult() }
+        val recommendations = doc.select("a.block").mapNotNull {
+            it.toSearchResult()
+        }
 
-        val numberRegex = Regex("\\d+")
+        val titRegex = Regex("\\d+")
         val episodes = ArrayList<Episode>()
-        
-        doc.select("section.container > div.border-b").forEach { seasonElement ->
-            val seasonText = seasonElement.select("button > span").text()
-            val seasonNumber = numberRegex.find(seasonText)?.value?.toInt()
-
-            seasonElement.select("div.season-list > a").forEach { episodeElement ->
-                val episodeHref = mainUrl + episodeElement.attr("href")
-                val episodeText = episodeElement.select("span.flex").text()
-                val episodeNumber = numberRegex.find(episodeText)?.value?.toInt()
-                val episodeName = episodeElement.ownText().trim()
-
+        doc.select("section.container > div.border-b").forEach { me ->
+            val seasonNum = me.select("button > span").text()
+            me.select("div.season-list > a").forEach {
                 episodes.add(
-                    newEpisode(episodeHref) {
-                        this.name = episodeName
-                        this.season = seasonNumber
-                        this.episode = episodeNumber
+                    newEpisode(mainUrl + it.attr("href"))
+                    {
+                        this.name=it.ownText().toString().removePrefix("Episode ").substring(2)
+                        this.season=titRegex.find(seasonNum)?.value?.toInt()
+                        this.episode=titRegex.find(it.select("span.flex").text().toString())?.value?.toInt()
                     }
                 )
             }
         }
-        
         return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
             this.posterUrl = poster
             this.plot = description
@@ -153,20 +156,28 @@ class NOXXProvider : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val doc = app.get(data, interceptor = ddosGuardKiller).document
-        
-        // Find all iframes that likely contain the video players
-        val iframes = doc.select("iframe[src]").map { it.attr("src").trim() }.filter { it.isNotBlank() }
-
-        if (iframes.isEmpty()) {
-            return false
+        val links = app.get(data, interceptor = ddosGuardKiller).document.select("div.h-vw-65 iframe.w-full").attr("src").toString()
+        val sourcelink= app.get(links, referer = mainUrl).document.selectFirst("iframe")?.attr("src")
+        if (sourcelink != null) {
+            val embedUrl = sourcelink.replace("/download/", "/e/")
+            val response = app.get(embedUrl, headers = mapOf("Accept-Language" to "en-US,en;q=0.9"))
+            val script = if (!getPacked(response.text).isNullOrEmpty()) {
+                getAndUnpack(response.text)
+            } else {
+                response.document.selectFirst("script:containsData(sources:)")?.data()
+            }
+            val m3u8 =
+                Regex("file:\\s*\"(.*?m3u8.*?)\"").find(script ?: "")?.groupValues?.getOrNull(1)
+            generateM3u8(
+                name,
+                m3u8 ?: "",
+                mainUrl
+            ).forEach(callback)
         }
-
-        // For each iframe found, let CloudStream's extractor system handle it.
-        iframes.forEach { iframeUrl ->
-            loadExtractor(iframeUrl, data, subtitleCallback, callback)
-        }
-
         return true
     }
+}
+
+class DoodPmExtractor : DoodLaExtractor() {
+    override var mainUrl = "https://dood.pm"
 }
