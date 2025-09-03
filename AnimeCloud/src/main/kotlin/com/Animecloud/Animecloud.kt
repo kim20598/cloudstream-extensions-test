4a9518280c708ab62ad5e49a5307eb39e4ce4a18
kim20598 <ki688494@gmail.com>
2025-09-03 18:49:47 +0100

    Update Animecloud.kt

--- AnimeCloud/src/main/kotlin/com/Animecloud/Animecloud.kt
+++ AnimeCloud/src/main/kotlin/com/Animecloud/Animecloud.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addImdbUrl
 
 class Animecloud : MainAPI() {
-    override var mainUrl              = "https://fireani.me"
+    override var mainUrl              = "https://kissanime.ba"
     override var name                 = "Animecloud"
     override val hasMainPage          = true
     override var lang                 = "de"
