7c2909a76344aa31324c690c101faf96c015206e
kim20598 <ki688494@gmail.com>
2025-10-10 11:50:16 +0100

    Add files via upload

---
+++ AkSv1/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1          // start at 1 – bump when you release
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        buildConfigField(
+            "String",
+            "AKSV_API",
+            "\"${properties.getProperty("AKSV_API", "https://ak.sv")}\""
+        )
+    }
+}
+
+dependencies {
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.6")
+}
+
+cloudstream {
+    language = "en"
+    authors  = listOf("kim20598")
+    status   = 1
+    tvTypes  = listOf("Anime", "OVA")
+    iconUrl  = "https://www.google.com/s2/favicons?domain=ak.sv&sz=%size%"
+    isCrossPlatform = false
+}
