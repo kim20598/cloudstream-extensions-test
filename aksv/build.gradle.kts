d8502c0044c3caba5b106e4417ed43cbbf8de60d
kim20598 <ki688494@gmail.com>
2025-10-10 01:32:07 +0000

    Add files via upload

--- aksv/build.gradle.kts
+++ aksv/build.gradle.kts
@@ -1,24 +1,22 @@
-plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm") version "1.9.24"
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1        // bump when you release
+
+android {
+    buildFeatures { buildConfig = true }
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        // ak.sv needs no secrets → leave empty or add later
+        buildConfigField("String", "AKSV_KEY", "\"\"")
+    }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://raw.githubusercontent.com/recloudstream/cloudstream/maven")
-}
-
-dependencies {
-    compileOnly("com.github.recloudstream:cloudstream:3.4.0")
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+cloudstream {
+    language    = "en"
+    authors     = listOf("kim20598")
+    status      = 1
+    tvTypes     = listOf("Anime", "OVA")
+    iconUrl     = "https://raw.githubusercontent.com/kim20598/cloudstream-extensions-test/master/aksv/icon.png"
+    isCrossPlatform = true
 }
